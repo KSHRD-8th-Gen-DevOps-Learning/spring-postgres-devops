75f37140e87ba7c0faf0352c1bd8bc57de62d750
Kimleang <kimleang.hrd@gmail.com>
2020-10-17 10:09:25 +0700

    Spring Postgresql DevOps

---
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.postgresdemo;
+
+import com.example.postgresdemo.storage.StorageProperties;
+import com.example.postgresdemo.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableConfigurationProperties(StorageProperties.class)
+public class PostgresDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresDemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
---
+++ src/main/java/com/example/postgresdemo/storage/StorageProperties.java
@@ -0,0 +1,20 @@
+package com.example.postgresdemo.storage;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+  @Value("files.server.location")
+  private String location;
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+}
